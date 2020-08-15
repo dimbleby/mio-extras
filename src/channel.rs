f8c5df7f82d9f42b8b411b88429fc310b981b378
David Hotham <david.hotham@metaswitch.com>
2020-08-15 18:15:37 +0100

    clippy

--- src/channel.rs
+++ src/channel.rs
@@ -166,9 +166,9 @@ impl<T> Clone for SyncSender<T> {
 impl<T> Receiver<T> {
     /// Attempts to return a pending value on this receiver without blocking.
     pub fn try_recv(&self) -> Result<T, mpsc::TryRecvError> {
-        self.rx.try_recv().and_then(|res| {
+        self.rx.try_recv().map(|res| {
             let _ = self.ctl.dec();
-            Ok(res)
+            res
         })
     }
 }
