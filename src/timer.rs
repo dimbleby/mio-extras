bc9b6a0f842333fed8e9db37e87af3c3b941fe7d
David Hotham <david.hotham@blueyonder.co.uk>
2024-07-21 12:46:43 +0100

    legacy numeric constants

--- src/timer.rs
+++ src/timer.rs
@@ -6,7 +6,7 @@ use slab::Slab;
 use std::sync::atomic::{AtomicUsize, Ordering};
 use std::sync::Arc;
 use std::time::{Duration, Instant};
-use std::{cmp, fmt, io, iter, thread, u64, usize};
+use std::{cmp, fmt, io, iter, thread};
 
 /// A timer.
 ///
